7a3bbcf31381fd7e21a0329b2ae9f1ff1e38779d
Beeformers <beeformers66@gmail.com>
2025-01-18 09:57:04 -0600

    Add files via upload
    Update 1

---
+++ BlockBuilder/BlockBuilder/BlockBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockBuilderApp.swift
+//  BlockBuilder
+//
+//  Created by Blue Dragon on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct BlockBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlockBuilder/BlockBuilder/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BlockBuilder
+//
+//  Created by Blue Dragon on 1/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
